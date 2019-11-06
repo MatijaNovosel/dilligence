192b5bfeb13ac2854d3081e20acf8752b5af990d
Matija Novosel <mnovosel@omega-software.hr>
2019-11-06 14:45:32 +0100

    Added DB script

--- tvz2api/Startup.cs
+++ tvz2api/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using tvz2api.Models;
 
 namespace tvz2api
 {
@@ -26,6 +28,7 @@ namespace tvz2api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<tvz2Context>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
