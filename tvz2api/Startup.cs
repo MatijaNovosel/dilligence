3e97378db76b3cdb9d2c733507529c0d7b05ad77
MatijaNovosel <mnovosel5@gmail.com>
2019-12-17 16:11:14 +0100

    Startup changed

--- tvz2api/Startup.cs
+++ tvz2api/Startup.cs
@@ -52,20 +52,8 @@ namespace tvz2api
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo {
                     Version = "v1",
-                    Title = "ToDo API",
-                    Description = "A simple example ASP.NET Core Web API",
-                    TermsOfService = new Uri("https://example.com/terms"),
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Shayne Boyer",
-                        Email = string.Empty,
-                        Url = new Uri("https://twitter.com/spboyer"),
-                    },
-                    License = new OpenApiLicense
-                    {
-                        Name = "Use under LICX",
-                        Url = new Uri("https://example.com/license"),
-                    }
+                    Title = "TVZ2API",
+                    Description = "TVZ2API"
                 });
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
