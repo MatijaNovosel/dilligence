22d7ae2c8cc3e5ad503ea482dfe267f9d66d5e0d
Matija Novosel <mnovosel5@gmail.com>
2020-01-07 23:16:34 +0100

    Fixed router

--- hotel-app.spa/src/router.js
+++ hotel-app.spa/src/router.js
@@ -2,7 +2,6 @@ import VueRouter from 'vue-router';
 import routes from './routes';
 import goTo from 'vuetify/es5/services/goto';
 import store from '../src/store/store';
-import AuthService from '../src/services/api/auth';
 import jwt_decode from 'jwt-decode';
 
 const router = new VueRouter({
@@ -24,18 +23,22 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
   const token = store.getters.user.token;
   if (to.matched.some(record => record.meta.requiresAuth)) {
-    const decoded = jwt_decode(token);
-    const exp = decoded.exp * 1000; // Expires at (EXP)
-    // If the token is expired or nonexistent, log out and redirect to login
-    if(new Date().getTime() > exp || token == null) {
-      AuthService.logout();
+    if(token == null) {
       next({
         path: '/login',
-        params: { 
-          nextUrl: to.fullPath 
-        }
+        params: { nextUrl: to.fullPath }
       });
     } else {
+      const decoded = jwt_decode(token);
+      const exp = decoded.exp * 1000; // Expires at (EXP)
+      // If the token is expired, log out and redirect to login
+      if(new Date().getTime() > exp) {
+        store.dispatch('removeUserData');
+        next({
+          path: '/login',
+          params: { nextUrl: to.fullPath }
+        });
+      }
       next();
     }
   } else if (to.matched.some(record => record.meta.guest)) {
