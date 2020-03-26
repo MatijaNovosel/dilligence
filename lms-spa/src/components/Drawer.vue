828154e49f9a627c116cc1709a6b59707c249d1c
MatijaNovosel <mnovosel5@gmail.com>
2020-03-26 10:08:47 +0100

    Added chat to drawer, eng text instead of cro

--- lms-spa/src/components/Drawer.vue
+++ lms-spa/src/components/Drawer.vue
@@ -1,160 +1,160 @@
 <template>
-	<div>
-		<q-drawer :width="250" v-model="drawerOpen" show-if-above bordered content-class="drawer-bg">
-			<q-list dense>
-				<q-item class="q-my-md">
-					<q-item-section avatar>
-						<q-avatar size="40px" @click="editPictureDialog = true">
-							<img src="../assets/default-user.jpg" />
-						</q-avatar>
-					</q-item-section>
-					<q-item-section>
-						<q-item-label lines="1">{{ `${user.name} ${user.surname}` }}</q-item-label>
-						<q-item-label caption lines="2">
-							<span class="text-weight-bold">JMBAG:</span>
-							{{ user.jmbag }}
-						</q-item-label>
-					</q-item-section>
-				</q-item>
-				<q-separator />
-				<q-expansion-item
-					group="drawer"
-					dense
-					dense-toggle
-					class="text-weight-regular"
-					v-for="link in links"
-					:label="link.text"
-					:key="link.text"
-				>
-					<q-list :key="i" dense v-for="(sublink, i) in link.sublinks">
-						<q-item @click="redirect(sublink.route)" class="text-caption" clickable v-ripple>
-							<span class="q-mt-xs q-pl-md">{{ sublink.text }}</span>
-						</q-item>
-					</q-list>
-				</q-expansion-item>
-			</q-list>
-		</q-drawer>
-		<q-dialog v-model="editPictureDialog" persistent>
-			<q-card class="picture-dialog">
-				<q-toolbar class="bg-primary text-white dialog-toolbar">
-					<span>Change profile picture</span>
-					<q-space />
-					<q-btn
-						:ripple="false"
-						dense
-						size="sm"
-						color="white"
-						flat
-						round
-						icon="mdi-close-circle"
-						@click="resetDialog"
-					/>
-				</q-toolbar>
-				<q-card-section class="text-center q-pb-none">
-					<q-img src="../assets/default-user.jpg" class="border-box-image"></q-img>
-				</q-card-section>
-				<q-card-section>
-					<q-file
-						accept=".jpg, .pdf, image/*"
-						dense
-						outlined
-						v-model="picture"
-						clearable
-						label="Upload picture"
-					>
-						<template v-slot:prepend>
-							<q-icon name="mdi-paperclip" />
-						</template>
-					</q-file>
-				</q-card-section>
-				<q-card-actions class="q-pt-none">
-					<q-space />
-					<q-btn :ripple="false" dense size="sm" color="primary">Upload</q-btn>
-				</q-card-actions>
-			</q-card>
-		</q-dialog>
-	</div>
+  <div>
+    <q-drawer :width="250" v-model="drawerOpen" show-if-above bordered content-class="drawer-bg">
+      <q-list dense>
+        <q-item class="q-my-md">
+          <q-item-section avatar>
+            <q-avatar size="40px" @click="editPictureDialog = true">
+              <img src="../assets/default-user.jpg" />
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label lines="1">{{ `${user.name} ${user.surname}` }}</q-item-label>
+            <q-item-label caption lines="2">
+              <span class="text-weight-bold">JMBAG:</span>
+              {{ user.jmbag }}
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-separator />
+        <q-expansion-item
+          group="drawer"
+          dense
+          dense-toggle
+          class="text-weight-regular"
+          v-for="link in links"
+          :label="link.text"
+          :key="link.text"
+        >
+          <q-list :key="i" dense v-for="(sublink, i) in link.sublinks">
+            <q-item @click="redirect(sublink.route)" class="text-caption" clickable v-ripple>
+              <span class="q-mt-xs q-pl-md">{{ sublink.text }}</span>
+            </q-item>
+          </q-list>
+        </q-expansion-item>
+      </q-list>
+    </q-drawer>
+    <q-dialog v-model="editPictureDialog" persistent>
+      <q-card class="picture-dialog">
+        <q-toolbar class="bg-primary text-white dialog-toolbar">
+          <span>Change profile picture</span>
+          <q-space />
+          <q-btn
+            :ripple="false"
+            dense
+            size="sm"
+            color="white"
+            flat
+            round
+            icon="mdi-close-circle"
+            @click="resetDialog"
+          />
+        </q-toolbar>
+        <q-card-section class="text-center q-pb-none">
+          <q-img src="../assets/default-user.jpg" class="border-box-image"></q-img>
+        </q-card-section>
+        <q-card-section>
+          <q-file
+            accept=".jpg, .pdf, image/*"
+            dense
+            outlined
+            v-model="picture"
+            clearable
+            label="Upload picture"
+          >
+            <template v-slot:prepend>
+              <q-icon name="mdi-paperclip" />
+            </template>
+          </q-file>
+        </q-card-section>
+        <q-card-actions class="q-pt-none">
+          <q-space />
+          <q-btn :ripple="false" dense size="sm" color="primary">Upload</q-btn>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 
 export default {
-	name: "Drawer",
-	props: ["drawerTrigger"],
-	data() {
-		return {
-			editPictureDialog: false,
-			picture: null,
-			drawerOpen: false,
-			links: [
-				{
-					icon: "mdi-bullhorn",
-					text: "Generalno",
-					route: { name: "/" },
-					sublinks: [
-						{
-							text: "Početna stranica",
-							route: { name: "home" }
-						},
-						{
-							text: "Popis zaposlenika",
-							route: { name: "employees" }
-						},
-						{
-							text: "Test",
-							route: { name: "test" }
-						}
-					]
-				},
-				{
-					icon: "mdi-file-document",
-					text: "Kolegiji",
-					route: { name: "subjects" },
-					sublinks: [
-						{
-							text: "Popis kolegija",
-							route: { name: "subjects" }
-						},
-						{
-							text: "Moji kolegiji",
-							route: { name: "my-subjects" }
-						}
-					]
-				},
-				{
-					icon: "mdi-test-tube",
-					text: "Zadatci",
-					route: { name: "exams" },
-					sublinks: [
-						{
-							text: "Exams",
-							route: { name: "exams" }
-						}
-					]
-				}
-			]
-		};
-	},
-	methods: {
-		redirect(route) {
-			if (this.$router.currentRoute.name === route.name) {
-				return;
-			}
-			this.$router.push(route);
-		},
-		resetDialog() {
-			this.editPictureDialog = false;
-			this.picture = null;
-		}
-	},
-	watch: {
-		drawerTrigger() {
-			this.drawerOpen = !this.drawerOpen;
-		}
-	},
-	computed: {
-		...mapGetters(["user"])
-	}
+  name: "Drawer",
+  props: ["drawerTrigger"],
+  data() {
+    return {
+      editPictureDialog: false,
+      picture: null,
+      drawerOpen: false,
+      links: [
+        {
+          icon: "mdi-bullhorn",
+          text: "General",
+          route: { name: "/" },
+          sublinks: [
+            {
+              text: "Home",
+              route: { name: "home" }
+            },
+            {
+              text: "Employees",
+              route: { name: "employees" }
+            },
+            {
+              text: "Chats",
+              route: { name: "chat" }
+            }
+          ]
+        },
+        {
+          icon: "mdi-file-document",
+          text: "Subjects",
+          route: { name: "subjects" },
+          sublinks: [
+            {
+              text: "Available subjects",
+              route: { name: "subjects" }
+            },
+            {
+              text: "Subscribed subjects",
+              route: { name: "my-subjects" }
+            }
+          ]
+        },
+        {
+          icon: "mdi-test-tube",
+          text: "Exams",
+          route: { name: "exams" },
+          sublinks: [
+            {
+              text: "Availabel exams",
+              route: { name: "exams" }
+            }
+          ]
+        }
+      ]
+    };
+  },
+  methods: {
+    redirect(route) {
+      if (this.$router.currentRoute.name === route.name) {
+        return;
+      }
+      this.$router.push(route);
+    },
+    resetDialog() {
+      this.editPictureDialog = false;
+      this.picture = null;
+    }
+  },
+  watch: {
+    drawerTrigger() {
+      this.drawerOpen = !this.drawerOpen;
+    }
+  },
+  computed: {
+    ...mapGetters(["user"])
+  }
 };
 </script>
