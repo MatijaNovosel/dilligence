1bba392d631dc173f35edb8a63a84f33ec7fbfb0
MatijaNovosel <mnovosel5@gmail.com>
2020-05-02 18:36:48 +0200

    Case sensitive components

---
+++ lms-spa/src/components/drawer.vue
@@ -0,0 +1,128 @@
+<template>
+  <div>
+    <q-drawer
+      :width="250"
+      v-model="drawerOpen"
+      show-if-above
+      :content-class="`drawer-bg${$q.dark.isActive ? '-dark' : ''} drawer-base`"
+    >
+      <q-list dense>
+        <q-item class="q-my-md">
+          <q-item-section avatar>
+            <q-avatar
+              size="45px"
+              style="cursor: pointer;"
+              @click="$router.push({ name: 'profile', params: { id: user.id } })"
+            >
+              <img
+                style="border: 1px solid rgba(0, 0, 0, 0.4)"
+                :src="user.picture == null ? require('../assets/default-user.jpg') : 'data:image/png;base64,' + user.picture"
+              />
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label lines="1">{{ `${user.name} ${user.surname}` }}</q-item-label>
+            <q-item-label caption lines="2">
+              <span class="text-weight-bold">{{ $i18n.t("username") }}:</span>
+              {{ user.username }}
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-separator />
+        <q-expansion-item
+          group="drawer"
+          dense
+          dense-toggle
+          class="text-weight-regular"
+          v-for="link in links"
+          :label="$i18n.t('drawerLinks.' + link.text)"
+          :key="link.text"
+        >
+          <q-list :key="i" dense v-for="(sublink, i) in link.sublinks">
+            <q-item @click="$router.push(sublink.route)" class="text-body2" clickable v-ripple>
+              <span class="q-pl-md">{{ $i18n.t('drawerLinks.' + sublink.text) }}</span>
+            </q-item>
+          </q-list>
+        </q-expansion-item>
+      </q-list>
+    </q-drawer>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "drawer",
+  props: ["drawerTrigger"],
+  data() {
+    return {
+      drawerOpen: false,
+      links: [
+        {
+          icon: "mdi-bullhorn",
+          text: "general",
+          route: { name: "/" },
+          sublinks: [
+            {
+              text: "home",
+              route: { name: "home" }
+            },
+            {
+              text: "employees",
+              route: { name: "employees" }
+            },
+            {
+              text: "chats",
+              route: { name: "chat" }
+            }
+          ]
+        },
+        {
+          icon: "mdi-file-document",
+          text: "courses",
+          route: { name: "courses" },
+          sublinks: [
+            {
+              text: "availableSubjects",
+              route: { name: "courses" }
+            }
+          ]
+        },
+        {
+          icon: "mdi-test-tube",
+          text: "exams",
+          route: { name: "exams" },
+          sublinks: [
+            {
+              text: "availableExams",
+              route: { name: "exams" }
+            }
+          ]
+        }
+      ]
+    };
+  },
+  watch: {
+    drawerTrigger() {
+      this.drawerOpen = !this.drawerOpen;
+    }
+  },
+  computed: {
+    ...mapGetters(["user"])
+  }
+};
+</script>
+
+<style lang="sass">
+.q-list--dense > .q-item, .q-item--dense
+  min-height: 19px
+.drawer-base
+  background-position: center center
+  background-size: cover
+  overflow: hidden
+.drawer-bg
+  background-image: url("../assets/nav-bg.svg")
+.drawer-bg-dark
+  background-image: url("../assets/nav-bg-dark.png")
+</style>
---
+++ lms-spa/src/components/navbar.vue
@@ -0,0 +1,78 @@
+<template>
+  <q-header elevated>
+    <q-toolbar :class="$q.dark.isActive ? 'bg-dark' : 'bg-white'">
+      <q-btn
+        flat
+        dense
+        round
+        :color="$q.dark.isActive ? 'white' : 'dark'"
+        icon="menu"
+        aria-label="Menu"
+        @click="$emit('drawerState')"
+      />
+      <q-img class="navbar-img q-ml-md" src="../assets/tvz-logo.svg"></q-img>
+      <span :class="($q.dark.isActive ? 'text-white' : 'text-black') + ' text-h6 q-ml-sm'">LMS</span>
+      <span
+        v-show="!$q.screen.xs && !$q.screen.sm"
+        class="text-grey q-ml-xs"
+      >{{ $i18n.t('author') }}</span>
+      <q-space />
+      <div
+        v-show="!$q.screen.xs && !$q.screen.sm"
+        class="text-subtitle1"
+        :class="($q.dark.isActive ? 'text-white' : 'text-black') + ' q-pr-md'"
+      >Quasar v{{ $q.version }}</div>
+      <notification-menu />
+      <q-btn
+        class="q-mx-sm"
+        flat
+        dense
+        round
+        color="grey-8"
+        icon="mdi-cog"
+        @click="settingsDialog = true"
+      />
+      <q-btn
+        flat
+        dense
+        round
+        class="q-mr-md"
+        color="red-7"
+        icon="power_settings_new"
+        @click="logout"
+      />
+      <settings-dialog :value="settingsDialog" @closed="settingsDialog = false" />
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import SettingsDialog from "../components/settings-dialog";
+import NotificationMenu from "../components/notification-menu";
+
+export default {
+  name: "navbar",
+  components: {
+    "settings-dialog": SettingsDialog,
+    "notification-menu": NotificationMenu
+  },
+  methods: {
+    ...mapActions(["removeUserData"]),
+    logout() {
+      this.removeUserData();
+      this.$q.notify({
+        type: "positive",
+        message: this.$i18n.t("successfullyLoggedOut")
+      });
+      this.$q.dark.set(false);
+      this.$router.push("/login");
+    }
+  },
+  data() {
+    return {
+      settingsDialog: false
+    };
+  }
+};
+</script>
